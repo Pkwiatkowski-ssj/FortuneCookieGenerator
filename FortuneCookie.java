eee03f5cedc677a2bf6b51cf1b3ce501717fa9b2
pkwiatkowski-ssj <pkwiatkowski@quinnipiac.edu>
2025-04-08 09:42:42 -0400

    added welcome message

--- FortuneCookie.java
+++ FortuneCookie.java
@@ -1,3 +1,16 @@
-public class FortuneCookie {
+import java.util.Scanner;
+import java.util.Random;
 
+public class FortuneCookie {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String[] fortunes = {
+            "Your kindness will lead you to unexpected places.",
+            "Today is a good day to try something new."
+        };
+
+        System.out.println("🥠 Welcome to the Fortune Cookie Generator!");
+        System.out.println("Press ENTER to crack open your cookie (or type 'exit' to quit).");
+    }
 }
